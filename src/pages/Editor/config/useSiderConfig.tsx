2b61ef0f40ec75e1c662fcd497c84a67859040d1
yiran <239559238@qq.com>
2022-10-09 17:05:38 +0800

    ✨ feat: useSiderConfig

---
+++ src/pages/Editor/config/useSiderConfig.tsx
@@ -0,0 +1,8 @@
+/*
+ *@description: 左侧菜单数据
+ *@author: zyc
+ *@date-time: 2022-10-09 16:36:32
+ */
+export const useSiderConfig = () => {
+  return [{}]
+}
---
+++ src/utils/still_label_editor/shape/select.ts
@@ -0,0 +1,7 @@
+/*
+ *@description: 选择工具
+ *@author: zyc
+ *@date-time: 2022-10-09 16:32:18
+ */
+
+export function select() {}
